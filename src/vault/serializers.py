0d6d776d188385825bfbba5d383bf9eceffee9a3
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2017-08-12 18:48:27 +0100

    :fire: Removing vault application.
    I decided to not make any REST API as managing security would be
    a f***ing pain and the data is too sensitive to let the user deal with
    all the security crap.

--- src/vault/serializers.py
+++
@@ -1,9 +0,0 @@
-from djangorestframework import serializers
-
-from vault import models
-
-
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Account
-        fields = '__all__'
